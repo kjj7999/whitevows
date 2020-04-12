cb151b222f30b48a664cb31dae08aceaab887e33
kjj7999 <kjj7999@gmail.com>
2020-04-13 00:58:12 +0900

    Spring Boot Initializer

---
+++ src/main/java/com/homefire/whitevows/WhitevowsApplication.java
@@ -0,0 +1,13 @@
+package com.homefire.whitevows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitevowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitevowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homefire/whitevows/WhitevowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homefire.whitevows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitevowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
